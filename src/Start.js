5824105f7751e22eace0acf80ad380df9d40e712
tushar2805garg <gargtushar460@gmail.com>
2021-05-15 19:11:06 +0530

    Changed router from  Browser To Hash

--- src/Start.js
+++ src/Start.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Front from "./Front";
 import App from "./App";
 function Start() {
